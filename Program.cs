8b734ce5879767f4960b64f748a35c21bbb3747b
Polina Kolesnikova <polina@example.com>
2023-02-23 16:48:14 +0400

    adding third exercise

--- Program.cs
+++ Program.cs
@@ -8,35 +8,48 @@
 
 // if (num1 > num2)
 // {
-//     Console.WriteLine("Большее число: " + num1);
+//     Console.WriteLine("The biggest number: " + num1);
 // }
 // else
 // {
-//     Console.WriteLine("Большее число: " + num2);
+//     Console.WriteLine("The biggest number: " + num2);
 // }
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 
-int max = 0;
-Console.Write("Enter first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-if(num1>max)
-{
-    max = num1;
-}
+// int max = 0;
+// Console.Write("Enter first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if(num1>max)
+// {
+//     max = num1;
+// }
 
-Console.Write("Enter second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if(num2>max)
-{
-    max = num2;
-}
+// Console.Write("Enter second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num2>max)
+// {
+//     max = num2;
+// }
 
-Console.Write("Enter third number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-if(num3>max)
-{
-    max = num3;
+// Console.Write("Enter third number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// if(num3>max)
+// {
+//     max = num3;
+// }
+// Console.WriteLine("The biggest number: " + max);
+
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Enter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2==0){
+    Console.WriteLine(num + " - even number");
 }
-Console.WriteLine("Максимальное из 3ех чисел: " + max);
+else
+{
+    Console.WriteLine(num + " - odd number");
+}
